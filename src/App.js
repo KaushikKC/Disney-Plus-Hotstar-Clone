630eb7546d796707edb34c56d584056d19f9d82b
KaushikKC <kccreations1704@gmail.com>
2022-01-03 21:51:04 +0530

    ADD: ImageSlider

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Counter } from './features/counter/Counter';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import ImgSlider from './components/ImgSlider';
 
 function App() {
   return (
